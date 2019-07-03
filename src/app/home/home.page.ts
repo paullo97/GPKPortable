f91b4a9acbb8bbd68ce8c5e722b774f726bdb154
paullo97 <paulo.cesar97@hotmail.com>
2019-07-03 20:26:28 -0300

    Reformulado arquivos e Controle de Paginas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl: NavController){}
+
+  SignIn(){
+    this.navCtrl.navigateRoot('sign-in');
+  }
+
+  SignUp(){
+    this.navCtrl.navigateRoot('sign-up'); 
+  }
 
 }
