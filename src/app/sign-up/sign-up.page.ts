ec8ef648042ed9bde5401ae57f577d4ca1bca50f
paullo97 <paulo.cesar@hotmail.com>
2019-07-04 14:19:31 -0300

    Tela Cadastro e layout

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
 })
-export class SignUpPage implements OnInit {
+export class SignUpPage{
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
-  ngOnInit() {
+  BackHome(){
+    this.navCtrl.navigateRoot('home');
   }
 
 }
