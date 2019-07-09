4c7c5ca1047a9b55579c19b8e1656d2c053dc2d0
paullo97 <paulo.cesar@hotmail.com>
2019-07-09 09:33:03 -0300

    Adicionado Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyD8LILrw6fBRCnC6DLuO_SqKeGAxZVB-Ts",
+      authDomain: "gpkportable.firebaseapp.com",
+      databaseURL: "https://gpkportable.firebaseio.com",
+      projectId: "gpkportable",
+      storageBucket: "gpkportable.appspot.com",
+      messagingSenderId: "678077580167",
+      appId: "1:678077580167:web:11b92a31cccf6002"
+    }),
+      AngularFireDatabaseModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/firebase-service.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseServiceService {
+
+  constructor(public db: AngularFireDatabase) {}
+}
